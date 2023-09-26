a5fe65a1a9e2f3bea1702d93de599b9820ac9f77
Armando Genis <a01654262@itesm.mx>
2023-09-25 21:01:48 -0600

    bicycle model added

--- Animation.cpp
+++ Animation.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include <chrono>
+#include <thread>
 #include <vector>
+#include <numeric>  // for std::iota
+
 #include "matplotlibcpp.h"
 #include "CubicSpline1D.h"
+#include "BicycleModel.h"
 
 using namespace std;
 namespace plt = matplotlibcpp;
@@ -23,13 +28,75 @@ void plot_waypoints( const std::vector<Eigen::VectorXd> waypoints){
 }
 
 
+void animation_car(const vector<Eigen::VectorXd> waypoints){
+    plt::ion();  
+    BicycleModel vehicle(0.0, 0.0, 0.0, 1.0);
+
+
+    std::vector<double> vehicle_x, vehicle_y, vehicle_theta;
+    double dt = 0.1;
+    for (int i = 0; i < 500; i++) {
+
+
+
+        cout << "vehicle x: " << vehicle.getX() << endl;
+        cout << "vehicle y: " << vehicle.getY() << endl;
+        cout << "Vehicle yaw: " << vehicle.getYaw() << std::endl;
+
+    
+        double velocity = vehicle.getV();
+
+
+        // Store vehicle data for plotting
+        vehicle_x.push_back(vehicle.getX());
+        vehicle_y.push_back(vehicle.getY());
+        vehicle_theta.push_back(vehicle.getTheta());
+
+        // Update vehicle state using bicycle model
+        
+        vehicle.update(0.3, 0.01, 0.1,0.523599); // delta (steering wheel), a (acceletarion), dt, max_steer = 30 deg
+
+        plt::clf();
+        vector<double> wp_x, wp_y;
+        for (const auto& waypoint : waypoints) {
+            wp_x.push_back(waypoint[0]);
+            wp_y.push_back(waypoint[1]);
+        }
+
+        plt::plot(wp_x, wp_y, "ro-");
+        plt::plot(vehicle_x, vehicle_y, "go");
+        
+        plt::named_plot("waypoints", wp_x, wp_y);
+        plt::named_plot("vehicle", vehicle_x, vehicle_y);
+        plt::legend();  // Show the legend to access the plot's properties
+        
+        plt::xlabel("X");
+        plt::ylabel("Y");
+        plt::title("Frenet Frame");
+        plt::grid(true);
+
+        plt::show();
+        try {
+            plt::pause(0.1);
+        } catch (const std::runtime_error& e) {
+            std::cerr << "Runtime error: " << e.what() << std::endl;
+        }
+
+    }
+
+
+    // Keep the plot window open
+    plt::show(true);
+    
+}
+
 
 int main() {
     // Given waypoints
     std::vector<double> x = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 6.0,6.0,6.0,6.0,6.0,6.0,6.0, 5.0,4.0,3.0,2.0,1.0,0.0,-1.0,-2.0,-3.0,-4.0,-5.0,-6.0,-7.0,-7.0,-7.0,-7.0,-7.0,-7.0,-7.0,-7.0, -6.0,-5.0,-4.0,-3.0,-2.0};
     std::vector<double> y = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0,2.0,3.0,4.0,5.0,6.0,7.0, 7.0,7.0,7.0,7.0,7.0,7.0,7.0,7.0,7.0,7.0,7.0,7.0,7.0, 6.0,5.0,4.0,3.0,2.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0};
     vector<Eigen::VectorXd> waypoints;
-    
+
     // Interpolate waypoints using CubicSpline1D
     // Generating a list of indices for the waypoints:
     std::vector<double> t_values(x.size());
@@ -45,14 +112,12 @@ int main() {
         Eigen::VectorXd waypoint(2);
         waypoint << spline_x.calc_der0(t), spline_y.calc_der0(t);
         waypoints.push_back(waypoint);
-        // x_new.push_back(spline_x.calc_der0(t));
-        // y_new.push_back(spline_y.calc_der0(t));
     }
 
     std::cout << "waypoints size: " << waypoints.size() << std::endl;
 
     // Plot interpolated waypoints
-    plot_waypoints(waypoints);
+    animation_car(waypoints);
 
     // #print the size of waypoints
     
--- README.md
+++ README.md
@@ -4,16 +4,18 @@
 ## Installation
 
  ```bash
-  sudo apt install libeigen3-dev
-  sudo apt install python3-matplotlib python3-dev
-  wget https://raw.githubusercontent.com/lava/matplotlib-cpp/master/matplotlibcpp.h
+sudo apt update
+sudo apt install g++
+sudo apt install libeigen3-dev
+sudo apt install python3-matplotlib python3-dev
+wget https://raw.githubusercontent.com/lava/matplotlib-cpp/master/matplotlibcpp.h
 ```
 
 ## Run Locally
 
 ```bash
-    g++ -I/usr/include/eigen3 -I/usr/include/python3.10 Animation.cpp CubicSpline1D.cpp -o frenet_animation -lpython3.10
-    ./frenet_animation
+g++ -I/usr/include/eigen3 -I/usr/include/python3.10 BicycleModel.cpp Animation.cpp CubicSpline1D.cpp -o frenet_animation -lpython3.10
+./frenet_animation
 ```
 
 
